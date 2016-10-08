70f98ff442c7b6b1c11f1911a2283954a340dc2f
Sniper <black_strings0@hotmail.com>
2016-10-07 23:09:30 -0500

    update

--- VectorLab/MyVector.h
+++ VectorLab/MyVector.h
@@ -75,63 +75,111 @@ private:
 
 /*
 
-#ifndef MYVECTOR_H
-#define MYVECTOR_H
+//-------------------- start of .h                                                  
+#ifndef VEC_H
+#define VEC_H
 
+template<class T>
+class Vec{
+ 
+ public:
+    int counter;
+    int cap;
+    T* arr;
+    
+    //constructor
+    Vec(int size);
+    ~Vec();
+    
+    bool canPush();
+    void show();
+    
+    void pop();
+    void push(T element);
+ 
+ 
+};
+#endif
+//------------------------- end of .h
+
+#include <iostream>
 #include <string>
 
 using namespace std;
 
+int main()
+{
+    //typedef Vec<int> MyVec;
+    //typedef Vec MyVec;
+    
+    Vec<string> v(5);
+    
+    v.push("I");
+    v.push("Am");
+    v.push("Cool");
+    v.pop();
+    
+    v.push("Super");
+    
+    v.show();
+    //cout << "ID: " << v.id << endl;
+    
+  //std::string name;
+  //std::cout << "What is your name? ";
+  //getline (std::cin, name);
+  std::cout << "Hello lets get started " << "!\n";
+}
+
+
 template<class T>
-class MyVector{
-
-public:
-	int counter;
-	int capacity;
-	T* arrPtr;
-
-	MyVector(int size){
-		counter = 0;
-		this->capacity = size;
-		arrPtr = new T[size];
-
-		//avoid looping and assigning all element to zero at start, for some reason don't work
-		//besides this vector is dynamci so you don't want to assign a type anyways because you don't know what type it will be
-		//for(int i=0; i<size; i++){
-		//	push(0);
-		//}
-		
-	}
-
-	void push(int n){
-		if(counter < capacity){
-			arrPtr[counter] = n;
-			counter++;
-		}
-	}
-
-	void pop(){
-		arrPtr[counter-1] = 0;
-		counter--;
-	}
-
-	void show(){
-		for(int i = 0; i<counter; i++){
-			cout << arrPtr[i] << ", ";
-		}
-		cout << endl;
-		cout << "Capacity: " << capacity << endl;
-		cout << "Current Size: " << counter << endl;
-	}
+Vec<T>::Vec(int size){
+    cap = size;
+    counter = 0;
+    arr = new T[size];
+}
 
 
-	~MyVector(){
-		//something
-		delete arrPtr;
-	}
+template<class T>
+Vec<T>::~Vec(){
+    counter = 0;
+}
 
-};
+template<class T>
+void Vec<T>::pop(){
+    
+    if(counter > 0){
+        //do not reset any, just remove the counter, since you can't replace c style array with new element
+        //arr[counter-1] = NULL;
+        cout << "popping" << endl; 
+        counter--;
+    }
+}
 
-//#endif
+template<class T>
+void Vec<T>::push(T element){
+    if(canPush()){
+        arr[counter] = element;
+        counter++;
+    }
+}
+
+template<class T>
+bool Vec<T>::canPush(){
+    if(counter >= cap){
+     return false;   
+    }
+    return true;
+}
+
+template<class T>
+void Vec<T>::show(){
+ cout << "size: " << cap << " - counter: " << counter << endl;
+ for(int i=0; i<counter; i++){
+  cout << arr[i] << ", ";   
+ }
+ cout << endl;
+}
+
+
 
 *\
